41d8354d60f8f2901c3796eeb51733560b459a1f
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-04-13 14:06:40 +0600

    installed nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello From My First Ever Server');
+    res.send('Hello From My First Every Server');
 })
 
 app.get('/data', (req,res) => {
